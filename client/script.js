7923bc28afeca1ee0bf1fb9112db963d8a4cea34
Grisha <voinich2008@mail.ru>
2021-12-05 15:09:12 +0300

    Update

---
+++ client/script.js
@@ -0,0 +1,11 @@
+var request = $.get("http://localhost:591/")
+request.done(function (data) {
+    var dict_quote = JSON.parse(data);
+    var title = "Цитата #" + dict_quote["number"]
+    var content = dict_quote["quote"]
+    $("title").text(title);
+    $("#text").text(content);
+});
+request.fail(function (jqXHR, textStatus, errorThrown) {
+    console.log(textStatus, errorThrown);
+});
